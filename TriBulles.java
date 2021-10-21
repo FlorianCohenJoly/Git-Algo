bed168ff99390cf267b14badab5cceed9d02b245
alan190902 <91255496+alan190902@users.noreply.github.com>
2021-10-21 13:38:46 +0200

    Add files via upload

---
+++ TriBulles.java
@@ -0,0 +1,34 @@
+public class TriBulles {
+
+    public void write (int [] tab) {
+        for (int i = 0; i< tab.length;i++){
+            System.out.print(tab[i] + "||");
+        }
+        System.out.println();
+    }
+    public void bulle (int [] tab){
+        int i,j,val;
+        for(i= tab.length - 1; i >= 1 ; i--){
+            for (j=1;j <= i;j++){
+                if ( tab [ j - 1] > tab [j]){
+                    // permutation
+                    val= tab[j-1];
+                    tab[j-1]=tab[j];
+                    tab[j]=val;
+                }
+            }
+        }
+
+        write(tab);
+    }
+
+    public static void main ( String[] args){
+
+        int[] tab = {10,5,9,8,0,1,3,2,4};
+        TriBulles tb = new TriBulles();
+
+        tb.bulle(tab);
+
+
+    }
+}
