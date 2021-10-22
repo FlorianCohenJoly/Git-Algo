db3c2c15d440a3ebfa0f30ef708aa68c70e8bafb
Adje-Abed <91177632+Adje-Abed@users.noreply.github.com>
2021-10-22 14:21:06 +0200

    Update TriBullesOpti.java

--- TriBullesOpti.java
+++ TriBullesOpti.java
@@ -1,3 +1,39 @@
+Tri à bulles optimisé :
+ on declare la taille du tableau
+   Dans la premiere boucle
+Pour n , on lui donne la valeur tab
+on affiche la valeur de tab avant  le tri
+
+on affecte
+   val= tab
+   on affiche la valeur de tab après  le tri 
+
+on declare les variales i,j,Temp et val
+dans le deuxieme boucle 
+Pour i = taille du tableau - 1 et i < tab
+  on a i >= 1
+dans la deuxieme boucle on refait une boucle
+Pour j = 0 ; 
+  on a j < i 
+
+on permute les valeurs
+   on a  (j) > T(j+1)
+     temp = (j+1);
+     T(j+1)=T(j);
+     T(j)=temp;
+     index du tab est inferieux on 
+     permutte les 2 et on termine la boucle
+
+  puis on return la valeur de T
+
+
+
+
+
+
+
+
+
 public class TriBullesOpti {
     public static void main(String[] args) {
         int [] tab = {10, 8, 5, 3, 7, 20, 45, 2, 11, 16};
