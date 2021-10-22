ecc6b983e64bb1a1e092187919ad4944b34d2ca6
alan190902 <91255496+alan190902@users.noreply.github.com>
2021-10-22 09:23:33 +0200

    Update Insertion2.java

--- Insertion2.java
+++ Insertion2.java
@@ -1,3 +1,17 @@
+Tri par insertion : 
+
+exemple de tableau où n est la longueur du tableau et n = 5
+pour i = 0 à n-1 
+on fait prendre à (tab)i la valeur min donc min=(tab)i
+on a j=i-1
+tant que j > 0 et que (tab) j > min
+on a (tab) j +1 = (tab) j 
+puis (tab) j+1 = min
+échange des valeurs dans l'ordre croissant 
+
+
+
+
 public class Insertion2 {
         public static void main(String[] args) {
             int [] tab = { -2, 78, 10, 245, 14, 4, 18, 19, 55, 21, 74};
